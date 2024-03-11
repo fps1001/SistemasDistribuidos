b038aa52afa64c361e34ebd6f753a985835d31c5
F. Pisot <fps1001@alu.ubu.es>
2024-03-11 11:44:54 +0100

    S4: Pruebas en cliente con registry.list.

--- Sesion04/RMI-HolaMundo/src/main/java/es/ubu/lsi/Cliente.java
+++ Sesion04/RMI-HolaMundo/src/main/java/es/ubu/lsi/Cliente.java
@@ -25,9 +25,17 @@ public class Cliente {
 		try {
 		   Registry registry = LocateRegistry.getRegistry(host);
 		   // Resuelve el objeto remoto (la referencia a...)
-	 	   HolaMundo stub = (HolaMundo) registry.lookup("Hola");
-	 	   String respuesta = stub.decirHola();
-	       System.out.println("Respuesta del servidor remoto: " + respuesta);
+			//FPS con el método list se obtiene los nombres de los objetos del registro.
+			String[] nombres_objetos = registry.list();
+			for (String name : nombres_objetos) {
+				HolaMundo stub = (HolaMundo) registry.lookup(name);
+				String respuesta = stub.decirHola();
+				System.out.println("Respuesta del objeto " + name + ": " + respuesta);
+			}
+
+//	 	   HolaMundo stub = (HolaMundo) registry.lookup("Hola");
+//	 	   String respuesta = stub.decirHola();
+//	       System.out.println("Respuesta del servidor remoto: " + respuesta);
 		} 
 		catch (Exception e) {
 	    	System.err.println("Excepción en cliente: " + e.toString());
