6fcf4ffb9c0465ac0f4883c3a918622a0ccc054c
F. Pisot <fps1001@alu.ubu.es>
2024-03-11 11:24:01 +0100

    S4: Servidor.java ya genera 3 objetos.

--- Sesion04/RMI-HolaMundo/src/main/java/es/ubu/lsi/Servidor.java
+++ Sesion04/RMI-HolaMundo/src/main/java/es/ubu/lsi/Servidor.java
@@ -27,16 +27,29 @@ public class Servidor implements HolaMundo {
     public static void main(String args[]) {
 	
 		try {
-		    Servidor obj = new Servidor();
+		    Servidor objeto1 = new Servidor();
+			//Añado 2 objetos más
+			Servidor objeto2 = new Servidor();
+			Servidor objeto3 = new Servidor();
 		    
 		    // si no hereda de UnicastRemoteObject es necesario exportar
-	    	HolaMundo stub = (HolaMundo) UnicastRemoteObject.exportObject(obj, 0);
+	    	//HolaMundo stub = (HolaMundo) UnicastRemoteObject.exportObject(obj, 0);
+			//FPS Exportamos 3 objetos.
+			HolaMundo stub1 = (HolaMundo) UnicastRemoteObject.exportObject(objeto1, 0);
+			HolaMundo stub2 = (HolaMundo) UnicastRemoteObject.exportObject(objeto2, 0);
+			HolaMundo stub3 = (HolaMundo) UnicastRemoteObject.exportObject(objeto3, 0);
+
 
 		    // Liga el resguardo de objeto remoto en el registro
+			//FPS Solo un registro pero 3 vinculos de nombre a un objeto remoto en el registro RMI
 	    	Registry registro = LocateRegistry.getRegistry();
-	    	registro.bind("Hola", stub);
+	    	registro.bind("Hola", stub1);
+			registro.bind("Hola2", stub2);
+			registro.bind("Hola3", stub3);
+
 	
-	    	System.out.println("Servidor preparado");
+	    	//System.out.println("Servidor preparado");
+			System.out.println("Servidor preparado y tres objetos registrados.");
 		}
 		catch (Exception e) {
 		    System.err.println("Excepción de servidor: " + e.toString());
