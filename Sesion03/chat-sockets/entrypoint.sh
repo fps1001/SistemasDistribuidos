4e4675b056deb55efc9d823477174fa961cfb423
F. Pisot <fps1001@alu.ubu.es>
2024-03-25 11:06:52 +0100

    Docker funcionando. Práctica terminada :)

--- Sesion03/chat-sockets/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-# Script de entrada en contenedor cliente para pasarle los parámetros de servidor y apodo.
-
-
-#!/bin/sh
-# entrypoint.sh
-
-java es.ubu.lsi.client.ChatClientImpl $SERVER_ADDRESS $NICKNAME
