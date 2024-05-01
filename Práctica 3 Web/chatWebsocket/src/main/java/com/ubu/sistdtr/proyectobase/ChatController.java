b4456ec27e6b323d265e8b623080b3c0e354d405
F. Pisot <fps1001@alu.ubu.es>
2024-05-01 14:43:46 +0200

    Ya identifica usuario. Modificar estilos por usuario.

--- Práctica 3 Web/chatWebsocket/src/main/java/com/ubu/sistdtr/proyectobase/ChatController.java
+++ Práctica 3 Web/chatWebsocket/src/main/java/com/ubu/sistdtr/proyectobase/ChatController.java
@@ -1,9 +1,14 @@
 package com.ubu.sistdtr.proyectobase;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
@@ -13,4 +18,16 @@ public class ChatController {
     public Message processMessage(@Payload Message mensaje) {
         return mensaje;
     }
+
+    @GetMapping("/api/userinfo")
+    public ResponseEntity<UserDetails> getUserInfo(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            return ResponseEntity.ok(userDetails);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+
+
 }
