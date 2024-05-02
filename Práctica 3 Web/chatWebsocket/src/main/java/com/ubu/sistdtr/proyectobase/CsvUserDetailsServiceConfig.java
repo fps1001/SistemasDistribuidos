a794c4b46fafcf96da2773dcdf0bacb5eadd549e
F. Pisot <fps1001@alu.ubu.es>
2024-05-02 18:15:59 +0200

    Eliminación clase CustomUserDetails.java

--- Práctica 3 Web/chatWebsocket/src/main/java/com/ubu/sistdtr/proyectobase/CsvUserDetailsServiceConfig.java
+++ Práctica 3 Web/chatWebsocket/src/main/java/com/ubu/sistdtr/proyectobase/CsvUserDetailsServiceConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -29,23 +30,28 @@ public class CsvUserDetailsServiceConfig implements InitializingBean {
         try (BufferedReader reader = new BufferedReader(new FileReader(new ClassPathResource("users.csv").getFile()))) {
             String line = reader.readLine(); // Skip the header line
             while ((line = reader.readLine()) != null) {
-                String[] userData = line.split(",");
-                // Example data: Username, Id,Password,Level,Is_inclusive
-                String username = userData[0].trim();
-                String id = userData[1].trim();
-                String password = "{noop}" + userData[2].trim();
-                UserLevel level = UserLevel.fromString(userData[3].trim());
-                boolean isInclusive = Boolean.parseBoolean(userData[4].trim());
-
-                // Define los permisos que se asignan al usuario.
-                List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + "ADMIN"));
-
-                CustomUserDetails user = new CustomUserDetails(username, password, authorities, id, level, isInclusive);
+                UserDetails user = getUserDetails(line);
                 users.add(user);
+                System.out.println("Usuarios: " + users);
             }
         } catch (IOException e) {
             throw new RuntimeException("Failed to read user data from CSV file", e);
         }
+    }
+
+    private static UserDetails getUserDetails(String line) {
+        String[] userData = line.split(",");
+        // Example data: Username, Id,Password,Level,Is_inclusive
+        String username = userData[0].trim();
+        //String id = userData[1].trim();
+        String password = "{noop}" + userData[2].trim();
+        //UserLevel level = UserLevel.fromString(userData[3].trim());
+        //boolean isInclusive = Boolean.parseBoolean(userData[4].trim());
+
+        // Define los permisos que se asignan al usuario.
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + "ADMIN"));
+
+        return new User(username, password, authorities);
     }
 
     @Bean
